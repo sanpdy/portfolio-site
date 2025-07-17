b48881520577bce7aee03a5f12191b0acf4e4f13
Sankalp Pandey <82685501+sanpdy@users.noreply.github.com>
2025-07-17 13:26:14 -0500

    Updated about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,13 @@
 // src/components/About.tsx
+
 export default function About() {
   return (
     <section id="about" className="section">
       <div className="container">
         <h1 className="text-lg mb-2">
-        Software Engineer<br />
-        Undergrad @ University of Arkansas<br />
-        Deep Learning Researcher for Quantum Materials Science &amp; Computational Neuroscience<br />
+        AI Researcher + Software Engineer<br />
+        Honors CS + CE @ University of Arkansas<br />
+        Deep Learning Systems for Quantum Materials Science &amp; Computational Neuroscience<br />
         <span className="text-sm text-gray-400">Fayetteville, AR, USA</span>
         </h1>
       </div>
